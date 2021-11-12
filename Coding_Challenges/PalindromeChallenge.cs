b4c662ba24692dbf7245bf9b9dc524f8de6619b0
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-11-12 06:21:40 -0500

    first commit

---
+++ Coding_Challenges/PalindromeChallenge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+namespace Coding_Challenges
+{
+    public class PalindromeChallenge
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            Console.ReadLine();
+            //bool numCheck = int.TryParse(Console.ReadLine(), out int userInput);
+
+            //if (numCheck == true)
+            //    Console.WriteLine("Please enter a 6 digit number");
+        }
+
+        public PalindromeChallenge()
+        {
+        }
+    }
+}
