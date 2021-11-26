467e22889166d4ea9be98aded006da2a2d9a5cb2
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-11-26 14:00:40 -0500

    Modified Palindrome Challenge to fit function requirements

---
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/PalindromeChallenge.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Coding_Challenges
+{
+    public static class PalindromeChallenge
+    {
+
+
+        //Function to check is number or descendants are Palindrome
+        public static bool PalindromeDescendent(int num)
+        {
+            //Convert num to string
+            string currentNum = num.ToString();
+            
+            while (currentNum.Length >= 2)
+            {
+                //Check if current number is a Palingdrome
+                char[] numArray = currentNum.ToCharArray();
+                Array.Reverse(numArray);
+                bool palinCheck = currentNum == new string(numArray);
+
+                //Ends function if num is a palindrome
+                if (palinCheck)
+                {
+                    return true;
+                }
+                else if (!palinCheck)
+                {
+                    if (currentNum.Length == 2)
+                    {
+                        return false;
+                    }
+
+                    string[] childNum = new string[currentNum.Length];
+
+                    for (int i = 0; i < currentNum.Length; i += 2)
+                    {
+                        try
+                        {
+                            string iSum = (Int32.Parse(currentNum[i].ToString()) + Int32.Parse(currentNum[i + 1].ToString())).ToString();
+                            childNum[i] = iSum;
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine("Descendent Int Created");
+                        }
+                    }
+                    currentNum = string.Join("", childNum);
+                    currentNum.Trim();
+                }
+             
+            }
+            return false;
+
+        }
+    }
+}
---
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using static Coding_Challenges.PalindromeChallenge;
+
+namespace Coding_Challenges
+{
+    //Objective: Create a function that returns true if the number or any of its descendants are a palindrome.
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Obtain user Input and validate accordingly
+            Console.WriteLine("Please enter a number");
+
+            string userInput = Console.ReadLine();
+
+            //Validate that number is a num and it is even
+            bool numCheck = userInput.All(char.IsDigit);
+
+            if (numCheck == false)
+                Console.WriteLine("Please enter a 6 digit number");
+            else if (userInput.Length % 2 == 1 || userInput.Length < 2)
+            {
+                Console.WriteLine("Please enter a a number with at least two digits that is also even");
+            }
+
+            //Check if number contains a palindrome descendent
+                //Convert input to integer
+            int userNum = int.Parse(userInput);
+
+                //Run the function
+            bool palinCheck = PalindromeDescendent(userNum);
+            Console.WriteLine($"You entered {userInput}");
+
+            if(palinCheck)
+            {
+                Console.WriteLine("Your number or one of its descendants is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The number you entered doesnt contain a palindrome");
+            }
+       
+            
+        }
+      
+    }
+}
