cc5bb19226c636108fe7bc4b9e18f77b7eafe0bd
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-11-26 17:15:28 -0500

    Started working on Easy branch setting the initial user input for selected challenge

--- Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
@@ -1,48 +1,47 @@
 ﻿using System;
 using System.Linq;
-using static Coding_Challenges.PalindromeChallenge;
+using System.Collections.Generic;
+using static Coding_Challenges.EasyChallengesFunctions;
 
 namespace Coding_Challenges
 {
-    //Objective: Create a function that returns true if the number or any of its descendants are a palindrome.
     public class Program
     {
         public static void Main(string[] args)
         {
+            List<string> easyChallenges = new List<string> { "Month Converter", "FindMinMaxNum", "MultiplyAllIntbyLengthofArray" };
+
+            //Have user select the challenge they want to view
+            Console.WriteLine("Please select the challenge you would like to cover by entering the number");
+
+            foreach (string item in easyChallenges)
+            {
+                int i = 1;
+                Console.WriteLine($"{i}.{item}");
+                i++;
+            }
+
+            bool userSelect = int.TryParse(Console.ReadLine(), out int selectedChallenge);
+            selectedChallenge = selectedChallenge - 1;
+
+            if (userSelect && (selectedChallenge <= easyChallenges.Count - 1 && selectedChallenge > 0))
+            {
+                switch (easyChallenges)
+                {
+                    case easyChallenges[0]
+                    default:
+                }
+            }
+
             //Obtain user Input and validate accordingly
             Console.WriteLine("Please enter a number");
 
             string userInput = Console.ReadLine();
 
-            //Validate that number is a num and it is even
-            bool numCheck = userInput.All(char.IsDigit);
 
-            if (numCheck == false)
-                Console.WriteLine("Please enter a 6 digit number");
-            else if (userInput.Length % 2 == 1 || userInput.Length < 2)
-            {
-                Console.WriteLine("Please enter a a number with at least two digits that is also even");
-            }
 
-            //Check if number contains a palindrome descendent
-                //Convert input to integer
-            int userNum = int.Parse(userInput);
 
-                //Run the function
-            bool palinCheck = PalindromeDescendent(userNum);
-            Console.WriteLine($"You entered {userInput}");
-
-            if(palinCheck)
-            {
-                Console.WriteLine("Your number or one of its descendants is a palindrome");
-            }
-            else
-            {
-                Console.WriteLine("The number you entered doesnt contain a palindrome");
-            }
-       
-            
         }
-      
+
     }
 }
