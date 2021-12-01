72513b6406ea2c7ba71a91c24df44404035a7688
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-12-01 11:49:39 -0500

    Created function to Modify 24 hour time to binary interpretation

---
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/ExpertChallenges.cs
@@ -0,0 +1,158 @@
+﻿using System;
+namespace Coding_Challenges
+{
+    public static class ExpertChallenges
+    {
+
+
+        //Objective: Create a function that takes in a Int and returns a bool value indicating if the number itself or any of its descendents are a palindrome.
+        public static bool PalindromeDescendent(int num)
+        {
+            //Convert num to string
+            string currentNum = num.ToString();
+
+            while (currentNum.Length >= 2)
+            {
+                //Check if current number is a Palingdrome
+                char[] numArray = currentNum.ToCharArray();
+                Array.Reverse(numArray);
+                bool palinCheck = currentNum == new string(numArray);
+
+                //Ends function if num is a palindrome
+                if (palinCheck)
+                {
+                    return true;
+                }
+                else if (!palinCheck)
+                {
+                    if (currentNum.Length == 2)
+                    {
+                        return false;
+                    }
+                    string[] childNum = new string[currentNum.Length];
+                    for (int i = 0; i < currentNum.Length; i += 2)
+                    {
+                        try
+                        {
+                            string iSum = (Int32.Parse(currentNum[i].ToString()) + Int32.Parse(currentNum[i + 1].ToString())).ToString();
+                            childNum[i] = iSum;
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine("Descendent Int Created");
+                        }
+                    }
+                    currentNum = string.Join("", childNum);
+                    currentNum.Trim();
+                }
+            }
+            return false;
+        }
+
+
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+        //Objective: Given the time as a string return an array containing strings that shows the lights for each row of the clock. String Format Sample "12:59:59"
+        public static string[] BuildBinaryClock(string time)
+        {
+            //Split string into substrings using the preformatted delimiter
+            time = time.Replace(":", "");
+            
+            //Declare and initialize the string array to return
+            string[] binaryTime =
+            {
+            /*8*/    " 0 0 0",
+            /*4*/  " 00000",
+            /*2*/ "000000",
+            /*1*/ "000000"
+            };
+
+            //Iterate through each int in the time entered.
+            for (int i = 0; i < time.Length; i++)
+            {
+                //Convert char in string time to int
+                int timeNum = time[i] - '0';
+
+                //Declare a char array of the string index in binaryTime array
+                char[] binaryTimeIndex;
+
+                //Use switch statement to manipulate string to correct time
+                switch(timeNum)
+                {
+                    case 1:
+                        binaryTimeIndex = binaryTime[3].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[3]  = string.Concat(binaryTimeIndex);
+                        break;
+                    case 2:
+                        binaryTimeIndex = binaryTime[2].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[2] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 3:
+                        binaryTimeIndex = binaryTime[2].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[2] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[3].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[3] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 4:
+                        binaryTimeIndex = binaryTime[1].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[1] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 5:
+                        binaryTimeIndex = binaryTime[3].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[3] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[1].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[1] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 6:
+                        binaryTimeIndex = binaryTime[1].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[1] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[2].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[2] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 7:
+                        binaryTimeIndex = binaryTime[1].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[1] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[2].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[2] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[3].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[3] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 8:
+                        binaryTimeIndex = binaryTime[0].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[0] = string.Concat(binaryTimeIndex);
+                        break;
+                    case 9:
+                        binaryTimeIndex = binaryTime[0].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[0] = string.Concat(binaryTimeIndex);
+
+                        binaryTimeIndex = binaryTime[3].ToCharArray();
+                        binaryTimeIndex[i] = '1';
+                        binaryTime[3] = string.Concat(binaryTimeIndex);
+                        break;
+                }
+            }
+            return binaryTime;
+        }
+    }
+}
--- Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
@@ -1,47 +1,83 @@
 ﻿using System;
 using System.Linq;
-using static Coding_Challenges.PalindromeChallenge;
+using System.Collections.Generic;
+using static Coding_Challenges.ExpertChallenges;
 
 namespace Coding_Challenges
 {
-    //Objective: Create a function that returns true if the number or any of its descendants are a palindrome.
     public class Program
     {
         public static void Main(string[] args)
         {
-            //Obtain user Input and validate accordingly
-            Console.WriteLine("Please enter a number");
+            List<string> challenges = new List<string> { "Check if Number is a Palindrome", "Convert time to binary clock", "MultiplyAllIntbyLengthofArray" };
 
-            string userInput = Console.ReadLine();
-
-            //Validate that number is a num and it is even
-            bool numCheck = userInput.All(char.IsDigit);
-
-            if (numCheck == false)
-                Console.WriteLine("Please enter a 6 digit number");
-            else if (userInput.Length % 2 == 1 || userInput.Length < 2)
+            //Have user select the challenge they want to view
+            Console.WriteLine("Please select the challenge you would like to cover by entering the number");
+            int i = 1;
+            foreach (string item in challenges)
             {
-                Console.WriteLine("Please enter a a number with at least two digits that is also even");
+                Console.WriteLine($"{i}.{item}");
+                i++;
             }
 
-            //Check if number contains a palindrome descendent
-                //Convert input to integer
-            int userNum = int.Parse(userInput);
+            bool userSelect = int.TryParse(Console.ReadLine(), out int selectedChallenge);
+            selectedChallenge = selectedChallenge - 1;
 
-                //Run the function
-            bool palinCheck = PalindromeDescendent(userNum);
-            Console.WriteLine($"You entered {userInput}");
+            if (userSelect && (selectedChallenge <= challenges.Count - 1 && selectedChallenge >= 0))
+            {
+                string userInput;
+                switch (selectedChallenge)
+                {
+                    case 0:
+                        //Obtain user Input and validate accordingly
+                        Console.WriteLine("Please enter a number");
 
-            if(palinCheck)
-            {
-                Console.WriteLine("Your number or one of its descendants is a palindrome");
+                        userInput = Console.ReadLine();
+
+                        //Validate that number is a num and it is even
+                        bool numCheck = userInput.All(char.IsDigit);
+
+                        if (numCheck == false)
+                            Console.WriteLine("Please enter a 6 digit number");
+                        else if (userInput.Length % 2 == 1 || userInput.Length < 2)
+                        {
+                            Console.WriteLine("Please enter a a number with at least two digits that is also even");
+                        }
+
+                        //Check if number contains a palindrome descendent
+                        //Convert input to integer
+                        int userNum = int.Parse(userInput);
+
+                        //Run the function
+                        bool palinCheck = PalindromeDescendent(userNum);
+                        Console.WriteLine($"You entered {userInput}");
+
+                        if (palinCheck)
+                        {
+                            Console.WriteLine("Your number or one of its descendants is a palindrome");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The number you entered doesnt contain a palindrome");
+                        }
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Please enter a time in 24 hour format. i.e 12:59:59");
+                        userInput = Console.ReadLine();
+
+                        string[] binaryClock = BuildBinaryClock(userInput);
+
+                        Console.WriteLine($"Here is your time in binary format \n " +
+                            $"{binaryClock[0]}, \n {binaryClock[1]}, \n {binaryClock[2]}, \n {binaryClock[3]}");
+                        break;
+
+
+
+
+
+                }
             }
-            else
-            {
-                Console.WriteLine("The number you entered doesnt contain a palindrome");
-            }
-       
-            
         }
       
     }
