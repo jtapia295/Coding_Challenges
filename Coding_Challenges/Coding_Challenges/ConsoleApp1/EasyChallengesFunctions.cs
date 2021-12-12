00cc2e963e218522ee6b9d43bb7c98b0fb14fbd8
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-12-12 11:51:24 -0500

    Completed two new easy challenges

--- Coding_Challenges/Coding_Challenges/ConsoleApp1/EasyChallengesFunctions.cs
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/EasyChallengesFunctions.cs
@@ -11,8 +11,7 @@ namespace Coding_Challenges
         public static string MonthConverter(int num)
         {
             string[] allMonths = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-            int indexedNum = num - 1;
-            string selectedMonth = allMonths[indexedNum];
+            string selectedMonth = allMonths[num - 1];
             return selectedMonth;
         }
 
@@ -53,5 +52,45 @@ namespace Coding_Challenges
         }
 
 
+        //Objective: Create  a function that takes in an Int[] and returns the absolute sum of arrays. 
+
+    public static int GetAbsSum(int[] arr)
+    {
+            //int absSum = 0;
+            //arr = arr.Select(d => Math.Abs(d)).ToArray();
+            //foreach (int num in arr) { absSum += num; }
+            //return absSum;
+
+            return arr.Select(Math.Abs).Sum();
+            //Alt. Solution
+            //return arr.sum(n => Math.Abs(n));
+    }
+
+    //Objective: Create a function that takes in an Int and an exponent number and return the calculation
+    public static long CalculateExponent(long number, long exponent)
+        {
+            return (long)Math.Pow(number, exponent);
+        }
+
+    //Objective: Create a function that takes in two strings and determines the hamming difference
+    public static int HammingDistance(string str1, string str2)
+    {
+        return str2.Where((ch, i) => ch != str1[i]).Count(); 
+    }
+
+     //Objective: Create a function that takes a string name of a person and returns a string with the first and last name swapped.
+     public static string nameShuffle(string str)
+        {
+            return string.Join(" ", str.Substring(str.LastIndexOf(' ') +1), str.Substring(0, str.LastIndexOf(' ')));
+            //Alt. Solution
+            //return string.Join(" ", str.Split(' ').Reverse());
+        }
+
+        //Objective: Create a function that returns the s,aller number
+        public static string smallerNum(string n1, string n2)
+        {
+            if (n1.Select((n, i) => n2[i].GetType() == typeof(char) ? n.CompareTo(n2[i]) : -1).Sum() < 0) { return n1; } else { return n2; }
+        }
+        
     }
 }
--- Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
+++ Coding_Challenges/Coding_Challenges/ConsoleApp1/Program.cs
@@ -9,70 +9,84 @@ namespace Coding_Challenges
     {
         public static void Main(string[] args)
         {
-            List<string> easyChallenges = new List<string> { "Month Converter", "FindMinMaxNum", "MultiplyAllIntbyLengthofArray" };
+            //List<string> easyChallenges = new List<string> { "Month Converter", "FindMinMaxNum", "MultiplyAllIntbyLengthofArray" };
 
-            //Have user select the challenge they want to view
-            Console.WriteLine("Please select the challenge you would like to cover by entering the number");
-            int i = 1;
-            foreach (string item in easyChallenges)
-            {
-                Console.WriteLine($"{i}.{item}");
-                i++;
-            }
+            ////Have user select the challenge they want to view
+            //Console.WriteLine("Please select the challenge you would like to cover by entering the number");
+            //int i = 1;
+            //foreach (string item in easyChallenges)
+            //{
+            //    Console.WriteLine($"{i}.{item}");
+            //    i++;
+            //}
 
-            bool userSelect = int.TryParse(Console.ReadLine(), out int selectedChallenge);
-            selectedChallenge = selectedChallenge - 1;
+            //bool userSelect = int.TryParse(Console.ReadLine(), out int selectedChallenge);
+            //selectedChallenge = selectedChallenge - 1;
 
-            if (userSelect && (selectedChallenge <= easyChallenges.Count - 1 && selectedChallenge >= 0))
-            {
-                switch (selectedChallenge)
-                {
-                    case 0:
-                        Console.WriteLine("Please select a number for the month you would like to access, 1-12");
-                        bool numCheck = false;
-                        int selectedMonth = -1;
-                        numCheck = int.TryParse(Console.ReadLine(), out selectedMonth);
-                        while (numCheck == false || (selectedMonth > 12 || selectedMonth < 0))
-                        {
-                            Console.WriteLine("Please enter a value between 1-12");
-                            numCheck = int.TryParse(Console.ReadLine(), out selectedMonth);
-                        }
-                         Console.WriteLine($"You have accessed the month {MonthConverter(selectedMonth)}");
-                        break;
-                    
-                    case 1:
-                        Console.WriteLine("Please enter a list of numbers to parse");
-                        string userInput = Console.ReadLine();
-                        int it = 0;
-                        int[] numArray = new int[userInput.Length];
-                        foreach(char s in userInput)
-                        {
-                            //char representation of 0-9 are consecutive numbers. Subtracting the char number by char 0 will result in the int value
-                            int num = s - '0';
-                            Console.WriteLine(s);
-                            numArray[it] = num;
-                            it++;
-                        }
-                        Console.WriteLine($"Here is your min and max number from your list of numbers,{ FindMinMax(numArray)} "); 
-                        break;
+            //if (userSelect && (selectedChallenge <= easyChallenges.Count - 1 && selectedChallenge >= 0))
+            //{
+            //    switch (selectedChallenge)
+            //    {
+            //        case 0:
+            //            Console.WriteLine("Please select a number for the month you would like to access, 1-12");
+            //            bool numCheck = false;
+            //            int selectedMonth = -1;
+            //            numCheck = int.TryParse(Console.ReadLine(), out selectedMonth);
+            //            while (numCheck == false || (selectedMonth > 12 || selectedMonth < 0))
+            //            {
+            //                Console.WriteLine("Please enter a value between 1-12");
+            //                numCheck = int.TryParse(Console.ReadLine(), out selectedMonth);
+            //            }
+            //             Console.WriteLine($"You have accessed the month {MonthConverter(selectedMonth)}");
+            //            break;
 
-                    case 2:
-                        Console.WriteLine("Please enter a list of numbers to parse");
-                        string userNumber = Console.ReadLine();
-                        int it2 = 0;
-                        int[] numArray2 = new int[userNumber.Length];
-                        foreach (char s in userNumber)
-                        {
-                            //char representation of 0-9 are consecutive numbers. Subtracting the char number by char 0 will result in the int value
-                            int num = s - '0';
-                            Console.WriteLine(s);
-                            numArray2[it2] = num;
-                            it2++;
-                        }
-                        Console.WriteLine($"Here are all your numbers multiplied by the amount of numbers entered{ MultiplyAllbyLength(numArray2)} ");
-                        break;
-                }
-            }
+            //        case 1:
+            //            Console.WriteLine("Please enter a list of numbers to parse");
+            //            string userInput = Console.ReadLine();
+            //            int it = 0;
+            //            int[] numArray = new int[userInput.Length];
+            //            foreach(char s in userInput)
+            //            {
+            //                //char representation of 0-9 are consecutive numbers. Subtracting the char number by char 0 will result in the int value
+            //                int num = s - '0';
+            //                Console.WriteLine(s);
+            //                numArray[it] = num;
+            //                it++;
+            //            }
+            //            Console.WriteLine($"Here is your min and max number from your list of numbers,{ FindMinMax(numArray)} "); 
+            //            break;
+
+            //        case 2:
+            //            Console.WriteLine("Please enter a list of numbers to parse");
+            //            string userNumber = Console.ReadLine();
+            //            int it2 = 0;
+            //            int[] numArray2 = new int[userNumber.Length];
+            //            foreach (char s in userNumber)
+            //            {
+            //                //char representation of 0-9 are consecutive numbers. Subtracting the char number by char 0 will result in the int value
+            //                int num = s - '0';
+            //                Console.WriteLine(s);
+            //                numArray2[it2] = num;
+            //                it2++;
+            //            }
+            //            Console.WriteLine($"Here are all your numbers multiplied by the amount of numbers entered{ MultiplyAllbyLength(numArray2)} ");
+            //            break;
+
+
+
+            //}
+            //}
+            //-----------------------------------------------------------------------------------------------------------------------------------------------------
+            //Return the absolute sum challenge
+            //Console.WriteLine(GetAbsSum(new int[] { 2, -1, -3, 4, 8 }));
+
+            //------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Return the name by placing the last name in the first place. 
+            //Console.WriteLine(nameShuffle("Jose Tapia"));
+
+            //------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Return the smaller number
+            Console.WriteLine(smallerNum("1500","16"));
         }
 
     }
